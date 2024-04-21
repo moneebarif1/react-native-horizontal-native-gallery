3f89b28b7787b96dd2c202221ff2091e0f030e63
Moneeb Arif <moneebariffast@gmail.com>
2024-04-21 11:02:20 +0500

    chore: removing and cleaning

--- ios/HorizontalNativeGallery.mm
+++ ios/HorizontalNativeGallery.mm
@@ -1,8 +1,8 @@
 #import "React/RCTBridgeModule.h"
 
-@interface RCT_EXPORT_MODULE(HorizontalNativeGallery, NSObject)
+@interface RCT_EXTERN_MODULE(HorizontalNativeGallery, NSObject)
 
-RCT_EXPORT_METHOD(showLightbox:(NSArray<NSDictionary *> *)horizontalGalleryImages
+RCT_EXTERN_METHOD(showLightbox:(NSArray<NSDictionary *> *)horizontalGalleryImages
                  startingIndex:(NSInteger)startingIndex
                        details:(NSDictionary * _Nullable)details)
 @end
